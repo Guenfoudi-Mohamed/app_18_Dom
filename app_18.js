dc5f17931c98d4fd6aa45511ead677e4e11c08af
GUENFOUDI MOHAMED <mohamed.guenfoudi@hotmail.com>
2022-09-27 16:06:33 +0100

    Update app_18.js

--- app_18.js
+++ app_18.js
@@ -4,7 +4,9 @@ const items =  document.querySelectorAll('body #header .navbar nav .list li .ite
 const sections =  document.querySelectorAll('body main section');
 const header =  document.querySelector('body #header');
 for(let i = 0;i<items.length;i++){
-    items[i].addEventListener('click',function(){
+    items[i].addEventListener('click',function(e){
+        e.preventDefault();
+        console.log(this.getAttribute('href'));
         window.scrollTo(0,(sections[i].offsetTop - header.offsetHeight));
     });
 };
